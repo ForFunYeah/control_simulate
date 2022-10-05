1371084c85ed571447ca740b0ee81977cbc171bf
xzc <72334376+ForFunYeah@users.noreply.github.com>
2022-10-05 10:44:04 +0800

    Update lat_controller.cc

--- modules/control/controller/lat_controller.cc
+++ modules/control/controller/lat_controller.cc
@@ -758,7 +758,7 @@ double LatController::ComputeFeedForward(double ref_curvature) const {
         (wheelbase_*ref_curvature + kv*(v*v)*ref_curvature -
          matrix_k_(0,2) *
              (lr_/(2.0*cf_) -
-              (lf_/(2.0*cr_))*((mass_*(v*v))*ref_curvature/wheelbase_)) *
+              (lf_/(2.0*cr_))*((mass_*(v*v))*ref_curvature/wheelbase_))) *
         180 / M_PI * steer_ratio_ / steer_single_direction_max_degree_ * 100;
   }
 
